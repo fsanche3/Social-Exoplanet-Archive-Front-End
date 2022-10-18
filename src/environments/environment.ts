3696ff4bbef32d5a7e0a7941a4a5bbad12baec07
fsanche3 <fsanche3@oswego.edu>
2022-10-18 14:55:43 -0400

    elasticbeanstalk url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   withCredentials: false,
   baseUrl: "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+distinct+pl_name,hostname,sy_mnum,discoverymethod,disc_year,disc_facility,disc_telescope,pl_orbper,pl_rade,pl_radj,pl_masse,pl_massj,pl_orbeccen+from+ps&format=json",
   stageUrl: "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+distinct+pl_name,hostname,sy_mnum,discoverymethod,disc_year,disc_facility,disc_telescope,pl_orbper,pl_rade,pl_radj,pl_masse,pl_massj,pl_orbeccen+from+ps+where+pl_name+=+",
-  apiUrl: "http://localhost:8080/",
+  apiUrl: "http://socialexoplanetarchive.us-east-1.elasticbeanstalk.com/",
 
 };
 
